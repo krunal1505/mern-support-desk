3f6b922698bff478095dea78dd06ec33420e03a9
Krunal Raol <krnlraol@gmail.com>
2022-03-29 10:37:19 +0530

    Added login and register modules with JWT backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { check, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
+
+const User = require("../model/userModel");
 
 // @desc    Register a new user
 // @route   /api/users
@@ -21,10 +25,37 @@ router.post(
     const { name, email, password } = req.body;
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
+      return res.status(400).json({ message: errors.array() });
     }
 
-    res.send("Register Route");
+    // Find if user already exists
+    const userExists = await User.findOne({ email: email });
+    if (userExists) {
+      res.status(400).json({ message: "User already registered" });
+    }
+
+    // Hash Password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //Create User
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    if (user) {
+      res.status(201).json({
+        message: {
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          token: generateToken(user._id),
+        },
+      });
+    } else {
+      res.status(400).json({ message: "Something went wrong" });
+    }
   })
 );
 
@@ -34,8 +65,31 @@ router.post(
 router.post(
   "/login",
   asyncHandler(async (req, res) => {
-    res.send("Login Route");
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    // Check user and password match
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.status(200).json({
+        message: {
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          token: generateToken(user._id),
+        },
+      });
+    } else {
+      res.status(401).json({
+        message: "Invalid Credentials",
+      });
+    }
   })
 );
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,28 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const colors = require("colors");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+const PORT = process.env.PORT || 5000;
 
-const app = express()
+// Connect to DB
+connectDB();
+
+const app = express();
 
 // Add Middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Defining Routes
-const userRoutes = require('./routes/userRoutes')
+const userRoutes = require("./routes/userRoutes");
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Welcome to Support-Desk API'})
-})
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to Support-Desk API" });
+});
 
 // Routes
-app.use('/api/users', userRoutes)
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server started on Port :${PORT}`)
-})
+  console.log(`Server started on Port :${PORT}`);
+});
