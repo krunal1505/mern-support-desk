2445d2f1d306c655146529ef0ab8a8a6fba9f9e0
Krunal Raol <krnlraol@gmail.com>
2022-04-16 21:34:54 -0400

    Added close ticket functionality

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from "react";
 import { toast } from "react-toastify";
 import { useSelector, useDispatch } from "react-redux";
-import { getTicket, reset } from "../features/tickets/ticketSlice";
-import { useParams } from "react-router-dom";
+import { getTicket, reset, closeTicket } from "../features/tickets/ticketSlice";
+import { useParams, useNavigate } from "react-router-dom";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 
@@ -13,6 +13,7 @@ function Ticket() {
   const dispatch = useDispatch();
   const params = useParams();
   const { ticketId } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (isError) {
@@ -22,6 +23,12 @@ function Ticket() {
     dispatch(getTicket(ticketId));
     // eslint-disable-next-line
   }, [isError, message, ticketId]);
+
+  const onTicketClose = () => {
+    dispatch(closeTicket(ticketId));
+    toast.success("Ticket Closed");
+    navigate("/tickets");
+  };
 
   if (isLoading) {
     return <Spinner />;
@@ -44,12 +51,19 @@ function Ticket() {
         <h3>
           Date submitted: {new Date(ticket.createdAt).toLocaleString("en-us")}
         </h3>
+        <h3>Product: {ticket.product}</h3>
         <hr />
         <div className="ticket-desc">
           <h3>Description of Issue</h3>
           <p>{ticket.description}</p>
         </div>
       </header>
+
+      {ticket.status !== "closed" && (
+        <button onClick={onTicketClose} className="btn btn-block btn-danger">
+          Close Ticket
+        </button>
+      )}
     </div>
   );
 }
